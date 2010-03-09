c6844a3036818abbd3d555cd538c55e7ce522f25
loranger <loranger@free.fr>
2010-03-09 23:52:51 +0100

    [Fix] Allow underscore in selectors

--- tao/core/Selector.php
+++ tao/core/Selector.php
@@ -11,14 +11,14 @@
  **/
 class Selector
 {
-	
+
 	private $expression;
-	
+
 	function __construct($expression)
 	{
 		$this->expression = $expression;
 	}
-	
+
 	/**
 	 * Convert Selector to XPath expression
 	 *
@@ -29,22 +29,22 @@ class Selector
 	{
 		$xpath = './/'.$this->expression;
 		$xpath = preg_replace("/\[(?!\w+\()/", '[@', $xpath);
-		$xpath = preg_replace("/(\#([a-z][a-z0-9]+))/i", '[@id="$2"]', $xpath);
-		$xpath = preg_replace("/(\.([a-z][a-z0-9]+))/i", '[contains(@class,"$2")]', $xpath);
+		$xpath = preg_replace("/(\#([a-z][a-z0-9_]+))/i", '[@id="$2"]', $xpath);
+		$xpath = preg_replace("/(\.([a-z][a-z0-9_]+))/i", '[contains(@class,"$2")]', $xpath);
 		$xpath = preg_replace("/\s(\+|\s)+/", '/following-sibling::', $xpath);
 		$xpath = preg_replace("/\s(>|\s)*/", '/', $xpath);
 		$xpath = preg_replace("/\/\[/", '/*[', $xpath);
-		
+
 		// TODO Implement missing css3 selectors
 		/*
 			// http://www.w3.org/TR/css3-selectors/
 			// http://xmlfr.org/w3c/TR/xpath/
 			:empty() -> not(* or text())
-			:not(s) -> not(self::*[_s]) 
-			
+			:not(s) -> not(self::*[_s])
+
 		*/
 		return $xpath;
-	}	
+	}
 }
 
 ?>
